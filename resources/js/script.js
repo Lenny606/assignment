64124a2269d9b8484190905e0c56f6a1ca148ffe
Lenny606 <thomas.kravcik@gmail.com>
2023-07-05 20:53:54 +0200

    index page, custom script.js file

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+const getUsers = async () => {
+    const response = await fetch("/api/users");
+    //    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    //    data.forEach(element => {
+    //      const target = document.createElement("li");
+    //      target.innerHTML = element;
+    //      document.querySelector("#ul").appendChild(target)
+    //    });
+
+}
+
+console.log(getUsers());

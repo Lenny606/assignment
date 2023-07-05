28600e9118de4023043fb674e752af1789506fdc
Lenny606 <thomas.kravcik@gmail.com>
2023-07-05 20:55:28 +0200

    users seeder, create users migration

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use DB;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Dummy data for testing purposes
+        DB::table('users')->truncate();
+        
+        $users =
+        [
+            [   
+                'name' => "dummyname",                
+                'email' => "dummmy@dummyname.com",
+                'password' => Hash::make("password")
+                  
+            ],
+            [   
+                'name' => "dummyname2",                
+                'email' => "dummmy2@dummyname.com",
+                'password' => Hash::make("password")
+                  
+            ],
+            [   
+                'name' => "dummyname3",                
+                'email' => "dummmy3@dummyname.com",
+                'password' => Hash::make("password")
+                  
+            ] 
+            ];
+
+        foreach($users as $user) {
+            $new_user = new User;
+
+            $new_user->name = $user['name'];
+            $new_user->email = $user['email'];
+            $new_user->password = Hash::make('password');
+
+            $new_user->save();
+
+        }
+    }
+}
